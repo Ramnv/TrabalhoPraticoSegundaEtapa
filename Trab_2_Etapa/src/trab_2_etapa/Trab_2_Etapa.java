a41198777d1b20aa05112a0acb4bac3351b150d6
20181210031 <20181210031@pc-129392.passofundo.ifsul.local>
2019-10-25 22:03:15 -0300

    Foram adicionados testes condicionais aos botões

---
+++ Trab_2_Etapa/src/trab_2_etapa/Trab_2_Etapa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab_2_etapa;
+
+/**
+ *
+ * @author 20181210031
+ */
+public class Trab_2_Etapa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
